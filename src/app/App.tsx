e550b7498626f4237a23139088952adc90272cce
hdy86 <dyhong86@naver.com>
2026-08-26 16:17:37 +0900

    chore: style setting

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,14 +2,14 @@ import { useState } from 'react'
 import heroImg from './assets/hero.png'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <section id="center">
+    <div className="flex test">zz</div>
+      <section id="center" className="mt-20">
         <div className="hero">
           <img src={heroImg} className="base" width="170" height="179" alt="" />
           <img src={reactLogo} className="framework" alt="React logo" />
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
+import './index.css'
+import '@assets/scss/style.scss';
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import App from './app/App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
